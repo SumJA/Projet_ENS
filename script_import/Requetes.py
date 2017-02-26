2cec2c594dc06bae44495c9e80c7dfae92ae775d
ElisaDnr <denier.elisa@gmail.com>
2017-02-26 13:36:35 +0100

    Ajout du fichier "requetes" --> insertion des données dans la base de données --> pas encore tout fonctionnel

--- script_import/Requetes.py
+++ script_import/Requetes.py
@@ -1,94 +1,103 @@
-import mysql.connector
-#import get_data
+import mysql.connector, os
+from script_import import get_data
 
+path_directory = "/media/elisa/Windows/Users/UTILISATEUR/Documents/M1/S2/Projets/ProjetM1BioInfo.tar/ProjetM1BioInfo/ProjetM1BioInfo/Version1"
 
 class ImportData():
     # TODO : documentation
     def __init__(self, path):
-        self.getdata = GetData(path)
-        self.all_data = {}
-        self.cnx = mysql.connector.connect(user='', password='', host='', database='')
+        self.getdata = get_data.GetData(path)
+        self.cnx = mysql.connector.connect(user='elisa', password='*elisa*1995', host='127.0.0.1', database='projetENS')
         self.cursor = self.cnx.cursor()
 
-    def import_families(self, file):
-        families = self.getdata.extract_all_families()
-        i = 0
-        for family in families:
-            print(i)
-            i = i + 1
-            self.all_data = self.getdata.get_data_for_expression_file(file, family)
+    def import_families(self):
+        for fg in list(set(self.getdata._extract_families_names(self.getdata._alignments_path))):
+            add_family_name = ("INSERT INTO gene_family" "(gene_family_name)" "VALUES(%s);")
+            data_family_name = (fg,)
+            self.cursor.execute(add_family_name, data_family_name)
+
+    def import_genes(self):  # marche pas encore car faut récupérer nom des gènes
+        for family in list(set(self.getdata._extract_families_names(self.getdata._alignments_path))):
+            for key in self.getdata.extract_gene_and_alignment_for_family(family).keys():
+                add_gene_name_and_EnsemblID = ("INSERT INTO genes" "(Ensembl_ID)" "VALUES(%s);")
+                data_gene_name_and_EnsemblID = (key,)
+                self.cursor.execute(add_gene_name_and_EnsemblID, data_gene_name_and_EnsemblID)
+
+    def import_alignments(
+            self):  # marche pas car dans base de données c'est "VARCHAR 45" sauf que les alignments sont plus longs et que je n'arrive pas à y changer dans la bdd
+        for family in list(set(self.getdata._extract_families_names(self.getdata._alignments_path))):
+            for value in self.getdata.extract_gene_and_alignment_for_family(family).values():
+                add_alignments = ("INSERT INTO alignment" "(alignment_data)" "VALUES(%s);")
+                data_alignments = (value,)
+                self.cursor.execute(add_alignments, data_alignments)
+
+    def import_tree(
+            self):  # marche pas car dans base de données c'est "VARCHAR 45" sauf que les alignments sont plus longs et que je n'arrive pas à y changer dans la bdd
+        for family in list(set(self.getdata._extract_families_names(self.getdata._alignments_path))):
+            for tree in self.getdata.extract_tree(family):
+                add_tree = ("INSERT INTO tree" "(tree_data)" "VALUES(%s);")
+                data_tree = (tree,)
+                self.cursor.execute(add_tree, data_tree)
+
+    def import_expressions(self):  # marche pas encore car faut remplir les clés étrangères
+        for family in list(set(self.getdata._extract_families_names(self.getdata._alignments_path))):
+            directory = os.listdir(self.getdata._expression_level_path)
+            for element in directory:
+                if not os.path.isdir(element):
+                    if element.endswith("quant.tsv"):
+                        exps = self.getdata.extract_expression_levels(element, family)
+                        for exp in exps:
+                            length = exp[0]
+                            eff_length = exp[1]
+                            est_counts = exp[2]
+                            exp_level = exp[3]
+                            add_expression_data = (
+                            "INSERT INTO expression" "(length, eff_count, est_count, expression_level)" "VALUES(%s,%s,%s,%s)")
+                            data_expression_data = (length, eff_length, est_counts, exp_level,)
+                            self.cursor.execute(add_expression_data, data_expression_data)
+
+    def import_species(self):  # marche pas car duplication --> à gérer
+        for family in list(set(self.getdata._extract_families_names(self.getdata._alignments_path))):
+            for specie in self.getdata.extract_species_for_gene(family).values():
+                add_species = ("INSERT INTO species" "(species_namel)" "VALUES(%s);")  # erreur de frappe sur "namel" ^^
+                data_species = (specie[1],)
+                self.cursor.execute(add_species, data_species)
+
+    def import_organs_expressionMethod_condition(
+            self):  # marche pas encore car pour "state" ça récupère une liste : par exemple :  ['Control\n', 'Discret\n', 'Discret \n'] et du coup on prend lequel là dedans ?? mais on peut pas rentrer une liste dans la base de données
+        directory = os.listdir(self.getdata._expression_level_path)
+        for element in directory:
+            if not os.path.isdir(element):
+                if element.endswith("metadata.tsv"):
+                    exps_info = self.getdata.extract_expression_info(
+                        self.getdata._expression_level_path + "/" + element)
+                    add_organ = ("INSERT INTO organs" "(organ_name)" "VALUES(%s);")
+                    data_organ = (exps_info[0],)
+                    self.cursor.execute(add_organ, data_organ)
+                    add_expression_method = ("INSERT INTO expression_method" "(expression_method_name)" "VALUES(%s);")
+                    data_expression_method = (exps_info[1],)
+                    self.cursor.execute(add_expression_method, data_expression_method)
+                    add_condition = ("INSERT INTO conditions" "(dvp_stage, state)" "VALUES(%s, %s);")
+                    data_condition = (exps_info[2], exps_info[3],)
+                    self.cursor.execute(add_condition, data_condition)
+
+    def close_database(self):
+        self.cnx.commit()
+        self.cursor.close()
+        self.cnx.close()
 
 
-    family_name = dictionnaire[0]
-    nom_gene =
-    id_gene =
-    alignment =
-    tree =
-    expression =
-    species =
-    organ =
-    expression_method =
-    condition =
+def main():
+    print("test")
+    importdata = ImportData(path_directory)
+    getdata = get_data.GetData(path_directory)
+    #    importdata.import_families()
+    #    importdata.import_genes()
+    #    importdata.import_alignments()
+    #    importdata.import_tree()
+    #    importdata.import_expressions()
+    #    importdata.import_species()
+    importdata.import_organs_expressionMethod_condition()
+    importdata.close_database()
 
-    try:
-        for fg in family_gene:
-            if family_gene[fg] =! family_gene[fg-1]:
-                cursor.execute("""INSERT INTO gene_family(gene_family_name) VALUES(?)""", (family_gene))
-
-
-        for g in id_gene:
-            cursor.execute("""INSERT INTO genes(Ensembl_ID) VALUES(?)""", (id_gene))
-
-        for a in alignment:
-            cursor.execute("""INSERT INTO alignment(alignment_data) VALUES(?)""", (alignment))
-
-        for t in tree:
-            cursor.execute("""INSERT INTO tree(tree_data) VALUES(?)""", (tree))
-
-        for e in expression:
-            length = expression[0] #supposition de ma part
-            eff_count = expression[1]
-            est_count = expression[2]
-            expression_level = expression[3]
-            cursor.execute("""INSERT INTO expression(length, eff_count, est_count, expression_level) VALUES(?, ?, ?, ?)""", (length, eff_count, est_count, expression_level))
-
-        for s in species:
-            cursor.execute("""INSERT INTO species(species_name) VALUES(?)""", (species))
-
-        for o in organ:
-            cursor.execute("""INSERT INTO organs(organ_name) VALUES(?)""", (organ))
-
-        for em in expression_method:
-            cursor.execute("""INSERT INTO expression_method(expression_method_name) VALUES(?)""", (expression_method))
-
-        for c in condition:
-            dvp_stage = condition[0]
-            state = condition[1]
-            #je ne vois pas rop ce que c'est ces deux trucs mdr
-            cursor.execute("""INSERT INTO conditions(dvp_stage, state) VALUES(?, ?)""", (dvp_stage, state))
-
-
-    # Make sure data is committed to the database
-    cnx.commit()
-
-    except sqlite3.OperationalError as b:
-        print(b)
-    except Exception as e:
-        print(e)
-        conn.rollback()
-    finally:
-        conn.close()
-
-
-    cursor.close()
-    cnx.close()
-
-
-# TODO : replace with parse argument :
-# path to the directory with data to import :
-path_directory = "C:\\Users\sumja_000\Documents\COURS\Projet2\données\ProjetM1BioInfo\Version1"
-
-# Tests :
-data = GetData(path_directory)
-import_data = ImportData(path_directory)
-import_data.import_families("Mus_musculus.KidneyRestriction.quant.tsv")
+main()
--- script_import/get_data.py
+++ script_import/get_data.py
@@ -2,6 +2,7 @@ import platform
 import os
 from colorama import Fore  # color console module
 import argparse  # command-line parsing module
+import csv  # csv / tsv reader module
 
 # Get argument and parse them
 # TODO : tuto => https://docs.python.org/3/howto/argparse.html :
@@ -12,15 +13,21 @@ parser.parse_args()
 
 
 class GetData:
-    # TODO : documentation : see example of documentation at http://thomas-cokelaer.info/tutorials/sphinx/docstring_python.html
+    # FIXME : VERY IMPORTANT !!!!!! : fuite mémoire car ouverture des fichiers en cascade !! Oui j'ai codé comme de la merde :'( Se limiter à 68 familles pour le moment
+    # TODO : documentation : see example of documentation at
+    # http://thomas-cokelaer.info/tutorials/sphinx/docstring_python.html
     def __init__(self, path):
         # Get OS info :
         if platform.uname().system == "Windows":
-            self._sign = "\\"
+            self.__sign = "\\"
         else:
-            self._sign = "/"
+            self.__sign = "/"
         if self.check_path_info(path) == 0:
             self.path_directory = path  # The directory path
+            self._alignments_path = self._get_all_directories_path()[0]
+            self._expression_level_path = self._get_all_directories_path()[1]
+            self._tree_path = self._get_all_directories_path()[2]
+            self._seq2sp_path = self._get_all_directories_path()[3]
         self.data = {}  # Dictionnaire qui devra conternir toutes les données : ce sera un dictionnaire
                         # Avec en clé les noms de familles
 
@@ -33,24 +40,40 @@ class GetData:
         :return: 0 OR print an exception
         """
         try:
-            assert os.path.exists(path)
+            assert os.path.isdir(path)
+            return 0
+        except AssertionError as Ae:
+            print(Ae)
+            print(Fore.RED + "ERROR : Unable to find the directory : "
+                             "the directory does not exist OR no read permissions")
+
+    @staticmethod
+    def check_file_info(file):
+        # TODO : documentation
+        """
+        Check if a given path exists
+        :param path: str
+        :return: 0 OR print an exception
+        """
+        try:
+            assert os.path.exists(file)
             return 0
         except AssertionError as Ae:
             print(Ae)
             print(Fore.RED + "ERROR : Unable to open file : the file does not exist OR no read permissions")
 
-    def get_all_directories_path(self):
+    def _get_all_directories_path(self):
         # TODO : documentation
         """
-        :return: alignments, expression_levels, tree, seq2sp
+        :return: _alignments_path, expression_levels, _tree_path, _seq2sp_path
         """
-        alignments = self.path_directory + self._sign + "Alignments"
+        alignments = self.path_directory + self.__sign + "Alignments"
         self.check_path_info(alignments)
-        expression_levels = self.path_directory + self._sign + "ExpressionLevels"
+        expression_levels = self.path_directory + self.__sign + "ExpressionLevels"
         self.check_path_info(expression_levels)
-        tree = self.path_directory + self._sign + "GeneTrees"
+        tree = self.path_directory + self.__sign + "GeneTrees"
         self.check_path_info(tree)
-        seq2sp = self.path_directory + self._sign + "Seq2SpTab"
+        seq2sp = self.path_directory + self.__sign + "Seq2SpTab"
         self.check_path_info(seq2sp)
         return alignments, expression_levels, tree, seq2sp
 
@@ -70,67 +93,147 @@ class GetData:
                 l.append(file[:-11])
         return l
 
-    def _init_data(self):
+    def extract_all_families(self):
         # TODO : documentation
         """
         Initialize self.data a dictionary of data with all the family names
         :return: 0 if all went well
         """
-        alignments = self.get_all_directories_path()[0]  # TODO : pour le moment on ne prend en compte que les noms de familles dans le dossier Alignments. Essayé de le faire toutes les dossiers
-        family = self._extract_families_names(alignments)
+        f = {}
+        family = self._extract_families_names(self._alignments_path)
         for i in family:
-            self.data[i] = []
+            f[i] = None
+        return f
 
-    def init_gene_from_alignments_for_family(self, family_name): # TODO : faire en sorte que ça marche auto pour chaque famille. Ce sera fait à l'aide d'une nouvelle fonction éventuellement.
+    def extract_all_species(self, filename):
+        # TODO : documentation
+        l = []
+        file = self.path_directory + "\\" + filename
+        if self.check_file_info(file) == 0:
+            try:
+                i = 0
+                with open(file, 'r') as tsv:
+                    for line in csv.reader(tsv, delimiter="\t"):
+                        if i == 0:
+                            pass
+                        else:
+                            l.append(line)
+                        i += 1
+                return l
+            except OSError:
+                print(Fore.RED + "ERROR : Failed to open species metadata file : ", filename)
+
+    def extract_gene_and_alignment_for_family(self, family_name):
         # TODO : documentation
         """
         :param family_name: str => a family name
-        :return: gene_dict: dict => a dictionary of all the genes for the given family
+        :return: gene_name: dict => a dictionary of all the genes with their aligned sequence for the given family
         """
-        gene_name = []
-        self._init_data()  # initialize data dictionary with all the families name
-        alignments = self.get_all_directories_path()[0]
-        file = alignments + self._sign + family_name + ".fasta"
-        f = open(file, "r")
-        for line in f.readlines():  # TODO : gestion erreurs
-            if line[0] == ">":
-                gene_name.append(line[1:line.find("_")])
-            self.data[family_name] = gene_name
+        gene_name = {}
+        file = self._alignments_path + self.__sign + family_name + ".fasta"
+        try:
+            f = open(file, "r")
+            for line in f:
+                if line[0] == ">":
+                    gene = line[1:line.find("_")]
+                    line = next(f)
+                    seq = line.splitlines()
+                    gene_name[gene] = seq
             f.close()
-        return 0
+            return gene_name
+        except OSError:
+            print(Fore.RED + "ERROR : Failed to open alignment file : ", file)
 
-    # TODO : faire le lien entre un gène et une espèce => faire en sorte que dans self.data : famille est une clé de gène,
-    # TODO : suite : gène est une clé d'espèce, d'expression et d'alignement. L'alignement est une clé d'arbre.
-    # TODO: suite : expressio est une clé de condition, d'organe et de méthode d'expression.
+    def extract_tree(self, family_name):
+        # TODO : documentation
+        tree = []
+        file = self._tree_path + self.__sign + family_name + ".tree"
+        try:
+            f = open(file, "r")
+            for line in f:
+                tree.append(line)
+            f.close()
+            return tree
+        except OSError:
+            print(Fore.RED + "ERROR : Failed to open _tree_path file : ", file)
 
-    def get_organ(self, file):
+    def extract_species_for_gene(self, family_name):
+        # TODO : documentation
+        file = self._seq2sp_path + self.__sign + family_name + "_Seq2Sp.tsv"
+        genes = self.extract_gene_and_alignment_for_family(family_name)
+        try:
+            f = open(file, "r")
+            with open(file, 'r') as tsv:
+                for line in csv.reader(tsv, delimiter="\t"):
+                    gene = line[0][:-7]
+                    species = line[1]
+                    genes[gene].append(species)
+            return genes
+        except OSError:
+            print(Fore.RED + "ERROR : Failed to open alignment file : ", file)
+
+    def extract_expression_info(self, file):
         # TODO : documentation
         organ = None
-        f = open(file, "r")
-        for line in f.readlines():
-            if line[:19] == "RNA_seq_condition: ":
-                organ = line[19:]
-        f.close()
-        return organ
+        method = None
+        tool = None
+        l_condition = []
+        if self.check_file_info(file) == 0:
+            try:
+                f = open(file, "r")
+                for line in f.readlines():
+                    if line[:19] == "RNA_seq_condition: ":
+                        organ = line[19:]
+                    elif line[:23] == "Quantification_method: ":
+                        method = line[23:line.find("#")]
+                    elif line[:21] == "Quantification_tool: ":
+                        tool = line[21:]
+                    elif line[:22] == "RNA_seq_subcondition: ":
+                        l_condition.append(line[22:])
+                    elif line[:24] == "RNA_seq_condition_type: ":
+                        l_condition.append(line[24:])
+                    elif line[:27] == "RNA_seq_subcondition_type: ":
+                        l_condition.append(line[27:])
+                f.close()
+                return organ, method, tool, l_condition
+            except OSError:
+                print(Fore.RED + "ERROR : Failed to open expression metadata file : ", file)
+        else:
+            print(Fore.RED + " ERROR : Can't find expression metadata file : ", file)
 
-    def get_tree(self, family_name):
+    def extract_expression_levels(self, file, family):
         # TODO : documentation
-        gene_dict = {}
-        tree = []
-        genetree = self.get_all_directories_path()[2]
-        file = genetree + self._sign + family_name + ".tree"
-        f = open(file, "r")
-        for line in f:
-            tree = line
-        f.close()
-        return tree
+        done_family = []
+        i = 0
+        exp = []
+        filename = self._expression_level_path + self.__sign + file
+        if self.check_file_info(filename) == 0:
+            try:
+                with open(filename, 'r') as tsv:
+                    for line in csv.reader(tsv, delimiter="\t"):
+                        if i != 0:
+                            gene = line[0][:-7]
+                            while family == line[0][-6:]:
+                                length = line[1]
+                                eff_length = line[2]
+                                est_counts = line[3]
+                                exp_level = line[4]
+                                l = [length, eff_length, est_counts, exp_level]
+                                exp.append(l)
+                                genes = self.extract_species_for_gene(family)
+                                genes[gene].append(l)
+                                if family not in done_family:
+                                    tree = self.extract_tree(family)
+                                    self.data[family] = [tree, genes]
+                                break
+                        i += 1
+                return exp
+            except OSError:
+                print(Fore.RED + "ERROR : Failed to open expression file : ", file)
+        else:
+            print(Fore.RED + " ERROR : Can't find expression file : ", file)
 
-
-# TODO : replace with parse argument :
-# path to the directory with data to import :
-path_directory = "C:\\Users\sumja_000\Documents\COURS\Projet2\données\ProjetM1BioInfo\Version1"
-# Tests :
-data = GetData(path_directory)
-data.get_all_directories_path()
-data.init_gene_from_alignments_for_family("F00000")
-print(data.data)
+    def get_data_for_expression_file(self, file, family):
+        # TODO :documentation
+        self.extract_expression_levels(file, family)
+        return self.data
