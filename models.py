fb027d98c40db8fd3df7fd7a586358c92298857b
sumJA <javaid.sumaira@gmail.com>
2017-03-14 11:48:01 +0100

    ==> fichier models.py : Nettoyage des models utiliser par django pour gérer la base de données.

--- models.py
+++ models.py
@@ -1,17 +1,24 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
+"""
+This is a Django model module.
+* Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+Feel free to rename the models, but don't rename db_table values or field names.
+"""
 from __future__ import unicode_literals
 
 from django.db import models
 
 
+class GeneFamily(models.Model):
+    idgene_family = models.AutoField(primary_key=True)
+    gene_family_name = models.CharField(unique=True, max_length=45)
+
+    class Meta:
+        managed = False
+        db_table = 'gene_family'
+
+
 class Alignment(models.Model):
-    idalignment = models.AutoField(db_column='idAlignment')  # Field name made lowercase.
+    idalignment = models.AutoField(primary_key=True)  # Field name made lowercase.
     tree = models.TextField()
     gene_family_idgene_family = models.ForeignKey('GeneFamily', models.DO_NOTHING, db_column='gene_family_idgene_family')
 
@@ -21,8 +28,33 @@ class Alignment(models.Model):
         unique_together = (('idalignment', 'gene_family_idgene_family'),)
 
 
+class Species(models.Model):
+    idspecies = models.IntegerField(primary_key=True)
+    species_name = models.CharField(unique=True, max_length=45)
+    species_taxid = models.CharField(db_column='Species_Taxid', unique=True, max_length=45, blank=True, null=True)  # Field name made lowercase.
+    assembly_name = models.CharField(db_column='Assembly_name', max_length=45, blank=True, null=True)  # Field name made lowercase.
+    assembly_source = models.CharField(db_column='Assembly_source', max_length=45, blank=True, null=True)  # Field name made lowercase.
+    species_classification = models.TextField(db_column='Species_Classification', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'species'
+
+
+class Genes(models.Model):
+    idgenes = models.AutoField(primary_key=True)
+    genes_name = models.CharField(max_length=45)
+    ensembl_id = models.CharField(db_column='Ensembl_ID', unique=True, max_length=45)  # Field name made lowercase.
+    species_idspecies = models.ForeignKey('Species', models.DO_NOTHING, db_column='species_idspecies')
+    gene_family_idgene_family1 = models.ForeignKey(GeneFamily, models.DO_NOTHING, db_column='gene_family_idgene_family1')
+
+    class Meta:
+        managed = False
+        db_table = 'genes'
+
+
 class AlignedSequence(models.Model):
-    idaligned_sequence = models.AutoField(unique=True)
+    idaligned_sequence = models.AutoField(primary_key=True)
     sequence = models.TextField()
     alignment_idalignment = models.ForeignKey(Alignment, models.DO_NOTHING, db_column='Alignment_idAlignment')  # Field name made lowercase.
     genes_idgenes = models.ForeignKey('Genes', models.DO_NOTHING, db_column='genes_idgenes')
@@ -70,41 +102,6 @@ class Expressionlevel(models.Model):
         unique_together = (('length', 'eff_count', 'est_count', 'expression_level'),)
 
 
-class GeneFamily(models.Model):
-    idgene_family = models.AutoField(primary_key=True)
-    gene_family_name = models.CharField(unique=True, max_length=45)
-
-    class Meta:
-        managed = False
-        db_table = 'gene_family'
-
-
-class GeneHasExpression(models.Model):
-    idgene_has_expressioncol = models.AutoField(unique=True)
-    genes_idgenes = models.ForeignKey('Genes', models.DO_NOTHING, db_column='genes_idgenes')
-    expressionlevel_idexpressionlevel = models.ForeignKey(Expressionlevel, models.DO_NOTHING, db_column='expressionlevel_idexpressionlevel')
-    expression_method_idexpression_method = models.ForeignKey(ExpressionMethod, models.DO_NOTHING, db_column='expression_method_idexpression_method')
-    conditions_idconditions = models.ForeignKey(Conditions, models.DO_NOTHING, db_column='conditions_idconditions')
-    organs_idorgans = models.ForeignKey('Organs', models.DO_NOTHING, db_column='organs_idorgans')
-
-    class Meta:
-        managed = False
-        db_table = 'gene_has_expression'
-        unique_together = (('idgene_has_expressioncol', 'genes_idgenes', 'expressionlevel_idexpressionlevel', 'expression_method_idexpression_method', 'conditions_idconditions', 'organs_idorgans'),)
-
-
-class Genes(models.Model):
-    idgenes = models.AutoField(primary_key=True)
-    genes_name = models.CharField(max_length=45)
-    ensembl_id = models.CharField(db_column='Ensembl_ID', unique=True, max_length=45)  # Field name made lowercase.
-    species_idspecies = models.ForeignKey('Species', models.DO_NOTHING, db_column='species_idspecies')
-    gene_family_idgene_family1 = models.ForeignKey(GeneFamily, models.DO_NOTHING, db_column='gene_family_idgene_family1')
-
-    class Meta:
-        managed = False
-        db_table = 'genes'
-
-
 class Organs(models.Model):
     idorgans = models.AutoField(primary_key=True)
     organ_name = models.CharField(unique=True, max_length=45)
@@ -114,14 +111,16 @@ class Organs(models.Model):
         db_table = 'organs'
 
 
-class Species(models.Model):
-    idspecies = models.IntegerField(primary_key=True)
-    species_name = models.CharField(unique=True, max_length=45)
-    species_taxid = models.CharField(db_column='Species_Taxid', unique=True, max_length=45, blank=True, null=True)  # Field name made lowercase.
-    assembly_name = models.CharField(db_column='Assembly_name', max_length=45, blank=True, null=True)  # Field name made lowercase.
-    assembly_source = models.CharField(db_column='Assembly_source', max_length=45, blank=True, null=True)  # Field name made lowercase.
-    species_classification = models.TextField(db_column='Species_Classification', blank=True, null=True)  # Field name made lowercase.
+class GeneHasExpression(models.Model):
+    idgene_has_expressioncol = models.AutoField(primary_key=True)
+    genes_idgenes = models.ForeignKey('Genes', on_delete=models.CASCADE, db_column='genes_idgenes')
+    expressionlevel_idexpressionlevel = models.ForeignKey(Expressionlevel,on_delete=models.CASCADE, db_column='expressionlevel_idexpressionlevel')
+    expression_method_idexpression_method = models.ForeignKey(ExpressionMethod, on_delete=models.CASCADE, db_column='expression_method_idexpression_method')
+    conditions_idconditions = models.ForeignKey(Conditions, on_delete=models.CASCADE, db_column='conditions_idconditions')
+    organs_idorgans = models.ForeignKey('Organs', on_delete=models.CASCADE, db_column='organs_idorgans')
 
     class Meta:
         managed = False
-        db_table = 'species'
+        db_table = 'gene_has_expression'
+        unique_together = (('idgene_has_expressioncol', 'genes_idgenes', 'expressionlevel_idexpressionlevel', 'expression_method_idexpression_method', 'conditions_idconditions', 'organs_idorgans'),)
+
