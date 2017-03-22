0911a9751f256c47fa22641ea910295d11c84997
ElisaDnr <denier.elisa@gmail.com>
2017-03-22 08:10:01 +0100

    Modifications légères du css

--- HTML/urls.py
+++ HTML/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.search_data, name='index'),
     url(r'^results/(?P<item>.+)/(?P<typedata>.+)$', views.results, name='results'),
-    url(r'^Tree/(?P<item>.+)/(?P<typedata>.+)$', views.tree, name='Tree'),
+    url(r'^Tree/(?P<item>.+)/(?P<typedata>.+)$', views.Tree, name='Tree'),
+    url(r'^Align/(?P<item>.+)/(?P<typedata>.+)$', views.Align, name='Align'),
+    url(r'^All/(?P<item>.+)/(?P<typedata>.+)$', views.All, name='All'),
+    url(r'^Expr/(?P<item>.+)/(?P<typedata>.+)$', views.Expr, name='Expr'),
+
 ]
